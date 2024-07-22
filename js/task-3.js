dba8e5aed74919dc7d263771b5af165a10b3e753
sasha-korn <korniichukaleksandra@gmail.com>
2024-07-22 22:27:20 +0100

    pushing everything

---
+++ js/task-3.js
@@ -0,0 +1,33 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padStart(str) {
+    // this.#value = str + this.#value;
+    this.#value = str.concat("", this.#value);
+  }
+  padEnd(str) {
+    // this.#value = this.#value + str;
+    this.#value = this.#value.concat("", str);
+  }
+  padBoth(str) {
+    // this.#value = str + this.#value + str;
+    this.#value = str.concat("", this.#value).concat("", str);
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
